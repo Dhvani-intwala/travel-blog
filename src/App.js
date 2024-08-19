c17f278f53cfbe5ca8adec613d0cc9064ac1dd8c
Dhvani-Intwala <intdhvani2627@gmail.com>
2024-08-19 21:37:47 +0200

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './nav';
 
 function App() {
   return (
     <div className="App">
-    
+      <Navbar />
+      <h2>Welcome to the Travel Blog</h2>
     </div>
   );
 }
--- src/nav.js
+++ src/nav.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 
-const Nav = () => {
+const Navbar = () => {
   return (
-    <nav>
-      
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <h1>My Travel World</h1>
+      </div>
+      <ul className="navbar-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Destination</a></li>
+        <li><a href="#">Gallery</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
     </nav>
   );
-};
+}
 
-export default Nav;
+export default Navbar;
