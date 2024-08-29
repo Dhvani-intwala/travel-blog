564d2f483273585a3745733377b1f6f6768147f8
Dhvani-Intwala <intdhvani2627@gmail.com>
2024-08-29 15:29:52 +0200

    Added title and caption on the main image with style

--- src/home.js
+++ src/home.js
@@ -4,8 +4,10 @@ import mainImage from './assets/main.png';
 
 const Home = () => {
     return (
-        <div className="navbar-brand">
-             <img src={mainImage} alt="Main Image " className="main-image" />
+        <div className="container">
+             <img src={mainImage} alt="MainImage" className="main-image" />
+             <h1 className='centered-title'>Explore.Dream.Discover.</h1>
+             <h3 className='centered'>This is a world travel blog featuring beautiful destinations, new experiences, and hidden places around the globe.</h3>
         </div>
     );
 }
