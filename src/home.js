b2db8ca05b986a3b6b1923358b17467b286a918f
Dhvani-Intwala <intdhvani2627@gmail.com>
2024-09-16 21:04:36 +0200

    Added about section with style

--- src/home.js
+++ src/home.js
@@ -4,6 +4,7 @@ import mainImage from './assets/main1.jpg';
 import travelBlog from './assets/travel-blog.jpg';
 import destinationImg from './assets/Destination.webp'
 import aboutmeImg from './assets/aboutme.jpg'
+import bothImg from './assets/both-img.jpg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
@@ -16,22 +17,40 @@ const Home = () => {
                 <button type="button" className="exploring-btn">Start Exploring</button>
             </div>
             
-            <div className="mt-3">
-                <h1 className="featured-title"><span>Featured</span></h1>
-  
-                <div className="container mt-5">
+            <div className="mb-5 mt-5">
+                <h1 className="all-title"><span>Featured</span></h1>
+
+                <div className="container mb-5 mt-5">
                     <div className="row">
                         <div className="col-sm">
                             <img src={travelBlog} alt="Travel-blog img" className="travel-image" />
-                            <h3>Travel blog</h3>
+                            <h3 className="mt-3 title">Travel Blog</h3>
                         </div>
                         <div className="col-sm">
-                            <img src={destinationImg} alt="destination img" className="travel-image" /> 
-                            <h3>Destination</h3>
+                            <img src={destinationImg} alt="destination img" className="travel-image" />
+                            <h3 className="mt-3 title">Destination</h3>
                         </div>
                         <div className="col-sm">
                             <img src={aboutmeImg} alt="Aboutme img" className="travel-image" />
-                            <h3>About Me</h3>
+                            <h3 className="mt-3 title">About Me</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container mt-5">
+                    {/* <h1 className="all-title"><span>About</span></h1> */}
+                    <div className="about-flex position-relative">
+                        <div className="self-image">
+                            <img src={bothImg} alt="Aboutme img" className="both-image" />
+                        </div>
+                        <div className="text">
+                            <h1 className='beautiful-text mb-2'>Welcome to my travel blog!</h1>
+                            <p>
+                                Hello! I’m Dhvani Intwala, a travel lover with a passion for exploring the world. 
+                                Through this blog, I share my stories, experiences, and tips to help you discover 
+                                new places and cultures. Join me on this adventure as I travel to amazing destinations 
+                                and offer you insights on how to make the most of your trips.
+                            </p>
                         </div>
                     </div>
                 </div>
