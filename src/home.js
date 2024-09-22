bc3342af2ce790aa05ca4813add218423e55887d
Dhvani-Intwala <intdhvani2627@gmail.com>
2024-09-22 22:24:38 +0200

    Added aboutme section with css

--- src/home.js
+++ src/home.js
@@ -47,10 +47,28 @@ const Home = () => {
                             <h1 className='beautiful-text mb-2'>Welcome to my travel blog!</h1>
                             <p className='aboutme-text'>
                                 Hello! I’m Dhvani Intwala, a travel lover with a passion for exploring the world. 
-                                Through this blog, I share my stories, experiences, and tips to help you discover 
-                                new places and cultures. Join me on this adventure as I travel to amazing destinations 
-                                and offer you insights on how to make the most of your trips.
+                                Through this blog, I share my journey, offering a glimpse into the wonders I’ve 
+                                discovered, whether it’s hiking in remote mountain ranges, tasting street food in bustling markets,
+                                or wandering through ancient cities.
                             </p>
+                           <p className='aboutme-text'>
+                                I believe that travel is more than just visiting new places—it's about the experiences, the people you meet,
+                                and the stories that unfold along the way. I’m passionate about capturing those moments and turning them into
+                                guides, tips, and personal stories that I hope will inspire others to step out of their comfort zones and see
+                                the world from a different perspective.
+
+                            </p> 
+
+                            <p className='aboutme-text'>
+                                Whether you’re planning your first adventure or you're an experienced traveler looking for fresh ideas, my goal is to 
+                                provide you with inspiration and practical advice.
+                            </p>
+
+                            <p className='aboutme-text'>
+                                Join me as I continue this journey, and let’s explore the world together! Feel free to subscribe to my blog or drop me a message if you have any questions or want to share your own travel stories.
+                                Let’s make the world our playground!
+                            </p>
+
                         </div>
                     </div>
                 </div>
